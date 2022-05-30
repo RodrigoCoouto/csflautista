0b55fe0591afb03036095f66eae937d8adab7fa5
RodrigoCoouto <rodcoutocontas@gmail.com>
2022-05-30 00:05:14 -0300

    fix: .g app controller

--- lib/app_controller.g.dart
+++ lib/app_controller.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'app_controller.dart';
+part of 'app/app_controller.dart';
 
 // **************************************************************************
 // StoreGenerator
